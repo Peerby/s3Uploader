b22a948c5ffb861cb9c9b9780d6584dded0c22f4
Arco Mul <me@arcomul.nl>
2016-10-31 14:14:27 +0100

    Include feature to downsize images before uploading + rename a function

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
 # s3Uploader
-Module to upload files from the browser to an AWS S3 bucket
+
+Module to upload files from the browser to an AWS S3 bucket. Sorry, not much documentation yet.
+
+## Example
+
+```
+// The image we are updating
+var $img = $('html').find('img');
+
+// Create the uploader
+uploader = s3Uploader.create({
+    bucket: aup.bucket,
+    key: aup.filename,
+    AWSAccessKeyId: aup.s3Key,
+    acl: 'public-read',
+    policy: aup.s3PolicyBase64,
+    signature: aup.s3Signature,
+    contenttype: 'image/jpeg',
+    width: 500,
+    height: 500,
+    fileAdded: onFileAdded
+});
+
+$img.on('click', function (e) {
+    e.stopImmediatePropagation();
+    uploader.open();
+});
+
+function onFileAdded (file, src) {
+
+    // Set the thumbnail directly to give the user feedback
+    if (src) {
+        $img.attr('src', src);
+    } else {
+        uploader.toDataUrl(function (src) {
+            $img.attr('src', src);
+        });
+    }
+
+    // Start uploading to AWS
+    uploader.upload({
+        success: function (url) {
+            // Save url of uploaded image in database
+            $.ajax({
+                url: '/api/user/avatar',
+                type: 'POST',
+                data: { avatar: url },
+                success: function () {
+                    // Yay, done
+                },
+                error: function () {
+                    // Error on own server
+                }
+            });
+        },
+        error: function () {
+            // Uploader encountered an error
+        }
+    });
+
+}
+```
--- main.js
+++ main.js
@@ -36,13 +36,70 @@
             _data.append("signature", options.signature);
             _data.append("Content-Type", options.contenttype || options["Content-Type"]);
 
+            var blobFile;
+
             // Add a hidden file input which can be triggered to open a file dialog
             $('body').append('<input type="file" style="display: none;" />');
+
             var _input = $('body').children().last();
             _input.change(function (e) {
                 var file = _input[0].files[0];
-                _options.fileAdded(file);
+
+                // When there is no width and height given, don't scale the image
+                if (!_options.width || !_options.height) {
+                    _options.fileAdded(file);
+                    return;
+                }
+
+                // Convert the image to data url
+                toDataUrl(function (src) {
+                    $('body').append('<img src="" style="display: none;" />');
+                    var img = $('body').children().last().attr('src', src)[0];
+
+                    // Scale it
+                    var width = img.width;
+                    var height = img.height;
+                    if (width > height) {
+                        width = (options.height / height) * width;
+                        height = options.height;
+                    } else {
+                        height = (options.width / width) * height;
+                        width = options.width;
+                    }
+
+                    // Convert to blob
+                    $('body').append('<canvas style="display: none;"></canvas>');
+                    var canvas = $('body').children().last()[0];
+                    canvas.width = options.width;
+                    canvas.height = options.height;
+                    var ctx = canvas.getContext("2d");
+                    ctx.drawImage(img, -(width - options.width) / 2, -(height - options.height) / 2, width, height);
+                    var dataurl = canvas.toDataURL("image/png");
+                    var blobBin = atob(dataurl.split(',')[1]);
+                    var array = [];
+                    for(var i = 0; i < blobBin.length; i++) {
+                        array.push(blobBin.charCodeAt(i));
+                    }
+                    blobFile = new Blob([new Uint8Array(array)], {type: 'image/png'});
+
+                    _options.fileAdded(null, dataurl);
+                });
             });
+
+            var toDataUrl = function toDataUrl (callback) {
+                if (!_input) {
+                    return console.error('Uploader not initilized');
+                }
+                if (_input[0].files.length == 0) {
+                    return console.error('No files added to uploader');
+                }
+                var file = _input[0].files[0];
+                var reader = new FileReader();
+                reader.onload = function(e) {
+                    callback(e.target.result);
+                };
+                reader.readAsDataURL(file);
+            }
 
             return {
 
@@ -51,29 +108,16 @@
                  */
                 open: function () {
                     if (!_input) {
-                        return console.error('Uploader not initilized');
+                        return console.error('s3Uploader not initialized');
                     }
                     _input.trigger('click');
                 },
 
                 /**
-                 * Generates a thumbail based on the added file
+                 * Converts the image to a data url
                  * returns it usign a callback
                  */
-                thumbnail: function (callback) {
-                    if (!_input) {
-                        return console.error('Uploader not initilized');
-                    }
-                    if (_input[0].files.length == 0) {
-                        return console.error('No files added to uploader');
-                    }
-                    var file = _input[0].files[0];
-                    var reader = new FileReader();
-                    reader.onload = function(e) {
-                        callback(e.target.result);
-                    };
-                    reader.readAsDataURL(file);
-                },
+                toDataUrl: toDataUrl,
 
                 /**
                  * Upload the added image to AWS
@@ -89,7 +133,7 @@
                     }
 
                     var file = _input[0].files[0];
-                    _data.append("file", file);
+                    _data.append("file", blobFile ? blobFile : file);
 
                     var req = new XMLHttpRequest();
                     req.open("POST", 'https://' + _options.bucket + '.s3.amazonaws.com/', true);
@@ -108,4 +152,3 @@
 }(typeof define === 'function' && define.amd ? define : function (cb) {
     cb(require, exports, module);
 }));
-
