c9afd8049cdbbf1b17f8e44e6aaa3735a3225048
Arco Mul <me@arcomul.nl>
2016-11-01 14:22:21 +0100

    [fix] Uploading in Safari didnt work because of using the image directly to draw to a canvas after setting the src

--- main.js
+++ main.js
@@ -39,7 +39,7 @@
             var blobFile;
 
             // Add a hidden file input which can be triggered to open a file dialog
-            $('body').append('<input type="file" style="display: none;" />');
+            $('body').append('<input type="file" style="display:none" />');
 
             var _input = $('body').children().last();
             _input.change(function (e) {
@@ -53,36 +53,43 @@
 
                 // Convert the image to data url
                 toDataUrl(function (src) {
-                    $('body').append('<img src="" style="display: none;" />');
-                    var img = $('body').children().last().attr('src', src)[0];
-
-                    // Scale it
-                    var width = img.width;
-                    var height = img.height;
-                    if (width > height) {
-                        width = (options.height / height) * width;
-                        height = options.height;
-                    } else {
-                        height = (options.width / width) * height;
-                        width = options.width;
-                    }
-
-                    // Convert to blob
-                    $('body').append('<canvas style="display: none;"></canvas>');
+                    // Add invisible canvas of prefered height and width
+                    $('body').append('<canvas style="display:none"></canvas>');
                     var canvas = $('body').children().last()[0];
                     canvas.width = options.width;
                     canvas.height = options.height;
                     var ctx = canvas.getContext("2d");
-                    ctx.drawImage(img, -(width - options.width) / 2, -(height - options.height) / 2, width, height);
-                    var dataurl = canvas.toDataURL("image/png");
-                    var blobBin = atob(dataurl.split(',')[1]);
-                    var array = [];
-                    for(var i = 0; i < blobBin.length; i++) {
-                        array.push(blobBin.charCodeAt(i));
-                    }
-                    blobFile = new Blob([new Uint8Array(array)], {type: 'image/png'});
 
-                    _options.fileAdded(null, dataurl);
+                    var image = new Image();
+                    image.onload = function() {
+
+                        // Read image sizes, use this to scale it to the prefered size
+                        var width = image.width;
+                        var height = image.height;
+                        if (width > height) {
+                            width = (options.height / height) * width;
+                            height = options.height;
+                        } else {
+                            height = (options.width / width) * height;
+                            width = options.width;
+                        }
+                        ctx.drawImage(image, -(width - options.width) / 2, -(height - options.height) / 2, width, height);
+
+                        // Convert the canvas to dataurl and blobfile to be send
+                        // to the client using this module. So that it can be used
+                        // to upload it to a server for example
+                        var dataurl = canvas.toDataURL("image/png");
+                        var blobBin = atob(dataurl.split(',')[1]);
+                        var array = [];
+                        for(var i = 0; i < blobBin.length; i++) {
+                            array.push(blobBin.charCodeAt(i));
+                        }
+                        blobFile = new Blob([new Uint8Array(array)], {type: 'image/png'});
+
+                        _options.fileAdded(null, dataurl);
+                    };
+                    // Set the image object source, onload will be called afterwards
+                    image.src = src;
                 });
             });
 
